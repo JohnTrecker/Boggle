e43825dcad99bf7802edc34a3b6226a6549b5803
johntrecker <jttrecker@gmail.com>
2017-08-28 16:52:39 -0700

    initilizes repo

---
+++ main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const data = ['aaafrs','aaeeee','aafirs','adennn','aeeeem','aeegmu','aegmnn','afirsy','bjkqxz','ccenst','ceiilt','ceilpt','ceipst','ddhnot','dhhlor','dhlnor','dhlnor','eiiitt','emottt','ensssu','fiprsy','gorrvw','iprrry','nootuw'];
+
